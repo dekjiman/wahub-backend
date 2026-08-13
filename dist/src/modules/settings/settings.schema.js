9b1ce3bddcc8f412933123af2aa8f9fc4d59e9b1
luftinur <luftinurfahmi@gmail.com>
2026-08-13 08:30:55 +0700

    update

--- dist/src/modules/settings/settings.schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateSettingSchema = void 0;
-const zod_1 = require("zod");
-exports.updateSettingSchema = zod_1.z.object({
-    value: zod_1.z.string({ required_error: 'Value is required' }),
-});
